b54a7a5191edabd3ecd11c326076d64bf2b584b1
HarrisonSamoy <hsamoy98@gmail.com>
2020-11-18 00:58:39 -0500

    Search display

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from '../models/company';
 import { Product } from '../models/product';
+import { CompanyService } from '../service/company.service';
 import { ProductService } from '../service/product.service';
 
 @Component({
@@ -10,17 +12,27 @@ import { ProductService } from '../service/product.service';
 export class ShopComponent implements OnInit {
 
   products: Product[];
+  companys: Company[];
+  searchFormData: any = {};
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private companyService: CompanyService) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe( products => {
       this.products=products;
     });
+    this.companyService.getCompanies().subscribe( companys => {
+      this.companys=companys;
+    })
   }
 
   addToCart(code: String): void {
     alert(code);
+  }
+
+  search(): void {
+    console.log(this.searchFormData);
+    location.reload();
   }
 
 }
